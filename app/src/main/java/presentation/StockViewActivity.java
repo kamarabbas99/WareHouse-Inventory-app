c9c1d138aee464642dd3698dc1e8a06bcd660d17
Alex Lannon <lannona@myumanitoba.ca>
2022-03-01 21:04:42 -0600

    Implemented RecycleView for dynamic scrolling of item objects currently in stock

--- app/src/main/java/presentation/StockViewActivity.java
+++ app/src/main/java/presentation/StockViewActivity.java
@@ -1,20 +1,50 @@
 package presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.warehouseinventorysystem.R;
 
+import objects.Item;
+
 //Stock View Activity:
 //  Shows a list of every item currently in the database and displays their data
-//  Receives an array upon creation from database for every item, loops through it and dynamically adds data for each at runtime
-//  Uses RecycleView for dynamic scroll
+//  Receives an array upon creation from database for every item
+//  Uses RecycleView for dynamically adding objects to the view screen
+//  Uses ItemsAdapter, which loops through very item and prints it to the screen dynamically
 public class StockViewActivity extends AppCompatActivity {
+    //Holds every item object currently in the database
+    Item[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_view);
+
+        //Gets the recycler view in stock activity layout
+        RecyclerView itemRV = findViewById(R.id.itemRV);
+
+        //Gets the list of all item objects in database
+        items = itemTest();
+
+        //Creates new item adapter and passes it the array of items
+        ItemsAdapter adapter = new ItemsAdapter(items);
+
+        //Sets recycle view layout to vertical scroll, and attached adapter to recycle view to display items
+        itemRV.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        itemRV.setAdapter(adapter);
+    }
+
+    private Item[] itemTest() {
+        Item[] items = new Item[20];
+
+        for(int i = 0; i < items.length; i++) {
+            items[i] = new Item(i, "Bagles", "I like bagels", i, "Pkg", 6);
+        }
+
+        return items;
     }
 }
