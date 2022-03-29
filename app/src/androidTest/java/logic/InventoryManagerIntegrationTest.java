e0488c50b9d565b2e1e800440ce7298c10b752ff
CurtisMcQ <mcquarrc@myumanitoba.ca>
2022-03-29 13:13:10 -0500

    Add setup for InventoryManagerIntegrationTest class and fill out structure for this class

--- app/src/androidTest/java/logic/InventoryManagerIntegrationTest.java
+++ app/src/androidTest/java/logic/InventoryManagerIntegrationTest.java
@@ -15,11 +15,15 @@ import database.InventoryManagerPersistence;
 import database.ItemPersistence;
 import objects.Item;
 
-/*
-    PURPOSE
+/* INVENTORYMANAGERINTEGRATIONTEST
+PURPOSE: To test the integration between the logic layer and the data layer with respect to the
+InventoryManagers table.
+NOTES: This will perform tests in the DatabaseTest class.
  */
 public class InventoryManagerIntegrationTest {
     InventoryManagerPersistence imPersistence;
+    InventoryManagerAccessor imAccessor;
+    ItemPersistence itemPersistence; // used only in tear down to remove added data
     DatabaseManager dbManager;
     String dbFilePath;
 
@@ -36,37 +40,83 @@ public class InventoryManagerIntegrationTest {
             e.printStackTrace();
         }
         dbManager.setDBFilePath(dbFilePath);
-        // done setup database connection for integration test
         // endregionSet
         // region $inventoryManagerSetup
         imPersistence = DatabaseManager.getInventoryManagerPersistence();
         assertNotNull(imPersistence);
+        imAccessor = new InventoryManagerAccessor(imPersistence);
+        assertNotNull(imAccessor);
         // endregion
     }
 
     @After
     public void tearDown() throws Exception {
+        imPersistence.clearDB(); // clear all data add to the InventoryManagers table
+        itemPersistence.clearDB(); // clear all data add to the Items table
     }
 
     @Test
-    public void createItem() {
+    public void createNewItem() {
 
     }
 
     @Test
-    public void addItem() {
+    public void createExistingItem() {
+
+    }
+
+    @Test // tries to create an IDSO that is not an Item (should fail)
+    public void createNonItem(){
+
     }
 
     @Test
-    public void removeItem() {
+    public void addNewItem() {
     }
 
     @Test
-    public void getItem() {
+    public void addExistingItem(){
+
+    }
+
+    @Test // tries to add to a IDSO that is not an Item (should fail)
+    public void addNonItem(){
+
+    }
+
+    @Test
+    public void removeExistingItem() {
+
+    }
+
+    @Test
+    public void removeNonExistingItem() {
+
+    }
+
+    @Test // tries to remove from a IDSO that is not an Item (should fail)
+    public void removeNonItem() {
+
+    }
+
+    @Test
+    public void getExistingItem() {
+    }
+
+    @Test
+    public void getNonExistingItem() {
+    }
+
+    @Test // tries to get an IDSO that is not an Item (should fail)
+    public void getNonItem() {
     }
 
     @Test
     public void getAllItems() {
+    }
+
+    @Test
+    public void getAllFromEmptyDB() {
     }
 
     @Test
