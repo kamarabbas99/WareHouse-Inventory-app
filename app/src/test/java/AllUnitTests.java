afde0fd0fb07296b71063387020a7419be61c585
CurtisMcQ <mcquarrc@myumanitoba.ca>
2022-04-20 17:02:12 -0500

    Remove unused classes

--- app/src/test/java/AllUnitTests.java
+++ app/src/test/java/AllUnitTests.java
@@ -1,15 +1,11 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import logic.AccountAccesserTest;
-import logic.InventoryManagerIntegrationTest;
-import logic.ItemAccesserTest;
 import objects.AccountTest;
 import objects.ItemTest;
 import objects.TransactionTest;
 import logic.InventoryManagerTest;
 import objects.InventoryTest;
-import objects.Transaction;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
@@ -17,8 +13,6 @@ import objects.Transaction;
         ItemTest.class,
         TransactionTest.class,
         InventoryManagerTest.class,
-        AccountAccesserTest.class,
-        ItemAccesserTest.class,
         InventoryTest.class
 })
 public class AllUnitTests {
