563746573a57610838b506959cd54d77bd4a7c0c
Alex Lannon <lannona@myumanitoba.ca>
2022-04-20 16:46:17 -0500

    Added all system test file

---
+++ app/src/androidTest/java/AllSystemTests.java
@@ -0,0 +1,23 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import presentation.AccountCreationSystemTest;
+import presentation.AccountPermissionSystemTest;
+import presentation.AddItemSystemTest;
+import presentation.CreateInventorySystemTest;
+import presentation.CreateItemSystemTest;
+import presentation.StockInformationSystemTest;
+import presentation.ViewInventorySystemTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AccountCreationSystemTest.class,
+        AccountPermissionSystemTest.class,
+        AddItemSystemTest.class,
+        CreateInventorySystemTest.class,
+        StockInformationSystemTest.class,
+        ViewInventorySystemTest.class,
+        CreateItemSystemTest.class
+})
+public class AllSystemTests {
+}
