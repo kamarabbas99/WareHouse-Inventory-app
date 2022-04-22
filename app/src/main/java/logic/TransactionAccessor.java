c883d651758948776ed00deccdd56756fc20e0cd
yuvrajwaraich <yuvrajw2001@gmail.com>
2022-04-22 15:22:06 -0500

    Added try-catch to methods in logic layer

--- app/src/main/java/logic/TransactionAccessor.java
+++ app/src/main/java/logic/TransactionAccessor.java
@@ -3,6 +3,7 @@ package logic;
 import database.IDBLayer;
 import database.DatabaseManager;
 import database.TransactionPersistence;
+import database.PersistenceException;
 import objects.IDSO;
 import objects.Transaction;
 
@@ -22,47 +23,78 @@ public class TransactionAccessor {
     // method that returns the report of all the transactions for a specific account
     // found using the given account id
     public String getAccountTransactions(int accountID) {
-        Transaction[] transactions = TransactionDB.getAccountTransactions(accountID);
-        String report = getReport(transactions);
+        String report = "";
+
+        try {
+            Transaction[] transactions = TransactionDB.getAccountTransactions(accountID);
+            report = getReport(transactions);
+        } catch (final PersistenceException exception) {
+            exception.printStackTrace();
+        }
+
         return report;
     }
 
     // method that returns the report of all the transactions for a specific item
     // found using the given item id
     public String getItemTransactions(int itemID) {
-        Transaction[] transactions = TransactionDB.getItemTransactions(itemID);
-        String report = getReport(transactions);
+        String report = "";
+
+        try {
+            Transaction[] transactions = TransactionDB.getItemTransactions(itemID);
+            report = getReport(transactions);
+        } catch (final PersistenceException exception) {
+            exception.printStackTrace();
+        }
+
         return report;
     }
 
     // method that returns the report of all the transactions for a specific
     // inventory found using the given inventory id
     public String getInventoryTransactions(int inventoryID) {
-        Transaction[] transactions = TransactionDB.getInventoryTransactions(inventoryID);
-        String report = getReport(transactions);
+        String report = "";
+
+        try {
+            Transaction[] transactions = TransactionDB.getInventoryTransactions(inventoryID);
+            report = getReport(transactions);
+        } catch (final PersistenceException exception) {
+            exception.printStackTrace();
+        }
+
         return report;
     }
 
     // method that returns the report of all the transactions
     public String getAllTransactions() {
-        IDSO[] transactionsAsIDSO = TransactionDB.getDB();
-        Transaction[] transactions = new Transaction[transactionsAsIDSO.length];
+        String report = "";
 
-        for (int i = 0; i < transactionsAsIDSO.length; i++) {
-            transactions[i] = (Transaction) transactionsAsIDSO[i];
+        try {
+            IDSO[] transactionsAsIDSO = TransactionDB.getDB();
+            Transaction[] transactions = new Transaction[transactionsAsIDSO.length];
+
+            // stores all elements of transactionsAsIDSO in transactions after converting to
+            // Transaction
+            for (int i = 0; i < transactionsAsIDSO.length; i++) {
+                transactions[i] = (Transaction) transactionsAsIDSO[i];
+            }
+
+            report = getReport(transactions);
+        } catch (final PersistenceException exception) {
+            exception.printStackTrace();
         }
 
-        String report = getReport(transactions);
         return report;
     }
 
-    // method that returns a report string which is made from given transactions array
+    // method that returns a report string which is made from given transactions
+    // array
     private String getReport(Transaction[] transactions) {
         String report = "";
 
-        for (Transaction transaction : transactions) {
-            if (transaction != null) {
-                report += transaction.toString() + "\n\n";
+        for (int i = 0; i < transactions.length; i++) {
+            if (transactions[i] != null) {
+                report += transactions[i].toString() + "\n\n";
             }
         }
 
